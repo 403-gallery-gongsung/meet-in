5599a17d05e94886ed8fd9c73ef36740ea41e5be
shindd <nidor02@gmail.com>
2024-01-24 01:01:03 +0900

    :sparkles: hello world 모듈 내 기존 repository를 분리하여 신규 persist 모듈 추가

--- hello-world/repository/src/main/kotlin/com/gongsung/sample/SampleRepositoryAutoConfiguration.kt
+++ hello-world/repository/src/main/kotlin/com/gongsung/sample/SampleRepositoryAutoConfiguration.kt
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@Import(
+    SampleMemoryRepository::class
+)
 @EntityScan
 @EnableJpaRepositories
 @AutoConfiguration
