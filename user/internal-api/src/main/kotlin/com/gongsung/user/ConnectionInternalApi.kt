36d096261df558d260aef949ecf61f59e5aaa5a5
kcm0147 <kcm0147@hanmail.net>
2024-02-04 23:42:35 +0900

    :sparkles: Connection Internal API 생성

---
+++ user/internal-api/src/main/kotlin/com/gongsung/user/ConnectionInternalApi.kt
@@ -0,0 +1,54 @@
+package com.gongsung.user
+
+import com.gongsung.connection.command.ConnectionRequestCommandUseCase
+import com.gongsung.connection.query.ConnectionRequestQueryUseCase
+import com.gongsung.user.dto.ConnectionRequestDto
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/internal/v1/connection")
+class ConnectionInternalApi(
+    private val connectionRequestCommandUseCase: ConnectionRequestCommandUseCase,
+    private val connectionRequestQueryUseCase: ConnectionRequestQueryUseCase
+) {
+
+    @GetMapping("/{userId}")
+    fun getAllConnectionByUserId(
+        @PathVariable("userId") userId: Long
+    ): ResponseEntity<List<ConnectionRequest>> {
+        return connectionRequestQueryUseCase.getAllConnectionByUserId(UserIdentity.of(userId))
+            .let { ResponseEntity.ok(it) }
+    }
+
+    @GetMapping("/{fromUserId}/{toUserId}")
+    fun getConnectionRequest(
+        @PathVariable("fromUserId") fromUserId: Long,
+        @PathVariable("toUserId") toUserId: Long
+    ): ResponseEntity<ConnectionRequest> {
+        return connectionRequestQueryUseCase.getByFromUserIdAndToUserId(
+            UserIdentity.of(fromUserId),
+            UserIdentity.of(toUserId),
+        )?.let { ResponseEntity.ok(it) }
+            ?: ResponseEntity.internalServerError().build()
+    }
+
+    @PostMapping
+    fun createConnectionRequest(
+        @RequestBody connectionRequest: ConnectionRequestDto
+    ): ResponseEntity<ConnectionRequest> {
+        return connectionRequestCommandUseCase.create(
+            fromUserIdentity = UserIdentity.of(connectionRequest.fromUserId),
+            toUserIdentity = UserIdentity.of(connectionRequest.toUserId),
+        ).let { ResponseEntity.ok(it) }
+    }
+
+    @PutMapping
+    fun updateConnectionRequest(
+        @RequestBody connectionRequest: ConnectionRequestDto
+    ): ResponseEntity<ConnectionRequest> {
+        return connectionRequestCommandUseCase.update(
+            connectionRequest,
+        ).let { ResponseEntity.ok(it) }
+    }
+}
