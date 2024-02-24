442c460a4fba1b8dd6e9512aa34e401a1e9e77f0
kcm0147 <kcm0147@hanmail.net>
2024-02-24 14:43:09 +0900

    :fire: User 모듈의 CompanyFeignClient 제거(#17)

--- user/internal-api-client/src/main/kotlin/com/gongsung/user/client/company/dto/CompanyResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.gongsung.user.client.company.dto
-
-import com.gongsung.company.Company
-
-data class CompanyResponse(
-    override val id: Long,
-    override val name: String,
-    override val address: String
-) : Company
