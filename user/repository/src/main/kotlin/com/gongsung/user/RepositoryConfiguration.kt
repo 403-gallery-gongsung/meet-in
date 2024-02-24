52eb3bd66996e9484a27d211a4c57e27d0db593c
kcm0147 <kcm0147@hanmail.net>
2024-02-24 14:44:19 +0900

    :adhesive_bandage: JPAAuditing 추가

--- user/repository/src/main/kotlin/com/gongsung/user/RepositoryConfiguration.kt
+++ user/repository/src/main/kotlin/com/gongsung/user/RepositoryConfiguration.kt
@@ -1,9 +1,13 @@
 package com.gongsung.user
 
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EnableJpaRepositories
+@EnableJpaAuditing
 @EntityScan(basePackages = ["com.gongsung.user.entity"])
+@Configuration
 class RepositoryConfiguration {
 }
