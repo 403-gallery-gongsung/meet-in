0b61736d1ffe2e9861ec0ccd81485135c9fd3fb8
yeollow <ljy2784437@gmail.com>
2024-02-27 19:32:50 +0900

    📝:Role을 위한 AccountType 정의

--- auth/repository/src/main/kotlin/com/gongsung/auth/entity/AccountEntity.kt
+++ auth/repository/src/main/kotlin/com/gongsung/auth/entity/AccountEntity.kt
@@ -5,6 +5,8 @@ import com.gongsung.auth.AccountProps
 import com.gongsung.auth.AccountType
 import jakarta.persistence.DiscriminatorColumn
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -22,16 +24,20 @@ open class AccountEntity(
     val id: Long = JpaConstant.NOT_YET,
     override val loginId: String = "",
     override val password: String = "",
+
+    @Enumerated(EnumType.STRING)
+    override val type: AccountType = AccountType.FORBID,
 ) : Account {
     companion object {
         fun ofProps(accountProps: AccountProps) = AccountEntity(
             loginId = accountProps.loginId,
             password = accountProps.password,
+            type = accountProps.type
         )
     }
 
     override val accountIdentity: Long
-        get() = id ?: throw RuntimeException("There is no user")
+        get() = id
 }
 
 object JpaConstant {
